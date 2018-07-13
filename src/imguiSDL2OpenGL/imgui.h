947be877d467847dfddd0841e57f62bc840a772e
dormon <imilet@fit.vutbr.cz>
2018-07-13 03:49:26 +0200

    Initial commit

---
+++ src/imguiSDL2OpenGL/imgui.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include<memory>
+#include<imguiSDL2OpenGL/imguisdl2opengl_export.h>
+#include <imguiDormon/imgui.h>
+
+struct SDL_Window;
+typedef union SDL_Event SDL_Event;
+
+namespace imguiSDL2OpenGL{
+  class Imgui;
+  class ImguiImpl;
+}
+
+class IMGUISDL2OPENGL_EXPORT imguiSDL2OpenGL::Imgui{
+  public:
+    Imgui(SDL_Window*window);
+    ~Imgui();
+    void newFrame(SDL_Window*window);
+    void render(SDL_Window*window,void*context);
+    bool processEvent(SDL_Event* event);
+  protected:
+    friend class ImguiImpl;
+    std::unique_ptr<ImguiImpl>impl;
+};
+
